c8ac88234d072b541aa2d304924b0613d780afa7
yeb2Binfang <68700549+yeb2Binfang@users.noreply.github.com>
2021-05-16 18:38:50 -0400

    Update train.py

--- Project/Yolov1/scripts/train.py
+++ Project/Yolov1/scripts/train.py
@@ -1,44 +1,44 @@
-"""
-Main file for training Yolo model on Pascal VOC dataset
-"""
-
 import torch
 import torchvision.transforms as transforms
 import torch.optim as optim
-import torchvision.transforms.functional as FT
+import torchvision.transforms.functional as FT 
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from model import Yolov1
 from dataset import VOCDataset
-from utils import (
+from utils import(
+    intersection_over_union,
     non_max_suppression,
     mean_average_precision,
-    intersection_over_union,
     cellboxes_to_boxes,
     get_bboxes,
     plot_image,
     save_checkpoint,
-    load_checkpoint,
+    load_checkpoint
 )
 from loss import YoloLoss
 
 seed = 123
 torch.manual_seed(seed)
+# make sure same dataset loading
 
-# Hyperparameters etc. 
+
+# hyperparameters etc
 LEARNING_RATE = 2e-5
-DEVICE = "cuda" if torch.cuda.is_available else "cpu"
-BATCH_SIZE = 16 # 64 in original paper but I don't have that much vram, grad accum?
-WEIGHT_DECAY = 0
-EPOCHS = 1000
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+WEIGHT_DECAY = 0 # drop-out
+# youtube 1:22:00
+EPOCH = 100
 NUM_WORKERS = 2
 PIN_MEMORY = True
 LOAD_MODEL = False
-LOAD_MODEL_FILE = "overfit.pth.tar"
-IMG_DIR = "data/archive/images"
+LOAD_MODEL_FILE = "overfit.path.tar"
+IMAGE_DIR = "data/archive/images"
 LABEL_DIR = "data/archive/labels"
 
 
+# entire class requires further explainations
 class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
@@ -48,13 +48,15 @@ class Compose(object):
             img, bboxes = t(img), bboxes
 
         return img, bboxes
-
-
-transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
-
+    
+    
+# transform = Compose(transforms)
+# transform(transforms.Resize((448,448)),transforms.ToTensor)
+transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor()])
+print(transform)
 
 def train_fn(train_loader, model, optimizer, loss_fn):
-    loop = tqdm(train_loader, leave=True)
+    loop = tqdm(train_loader, leave=True) # progress bar
     mean_loss = []
 
     for batch_idx, (x, y) in enumerate(loop):
@@ -66,11 +68,10 @@ def train_fn(train_loader, model, optimizer, loss_fn):
         loss.backward()
         optimizer.step()
 
-        # update progress bar
-        loop.set_postfix(loss=loss.item())
+        # updata
+        loop.set_postfix(loss = loss.item())
 
     print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
-
 
 def main():
     model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
@@ -85,12 +86,15 @@ def main():
     train_dataset = VOCDataset(
         "data/archive/8examples.csv",
         transform=transform,
-        img_dir=IMG_DIR,
-        label_dir=LABEL_DIR,
+        img_dir=IMAGE_DIR,
+        label_dir=LABEL_DIR
     )
 
     test_dataset = VOCDataset(
-        "data/archive/test.csv", transform=transform, img_dir=IMG_DIR, label_dir=LABEL_DIR,
+        "data/archive/test.csv",
+        transform=transform,
+        img_dir=IMAGE_DIR,
+        label_dir=LABEL_DIR
     )
 
     train_loader = DataLoader(
@@ -99,7 +103,7 @@ def main():
         num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY,
         shuffle=True,
-        drop_last=True,
+        drop_last=False                         # DROP_LAST:    when num(remain data) < batch_size, stop training
     )
 
     test_loader = DataLoader(
@@ -108,40 +112,23 @@ def main():
         num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY,
         shuffle=True,
-        drop_last=True,
+        drop_last=False  
     )
 
-    for epoch in range(EPOCHS):
-        # for x, y in train_loader:
-        #    x = x.to(DEVICE)
-        #    for idx in range(8):
-        #        bboxes = cellboxes_to_boxes(model(x))
-        #        bboxes = non_max_suppression(bboxes[idx], iou_threshold=0.5, threshold=0.4, box_format="midpoint")
-        #        plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
-
-        #    import sys
-        #    sys.exit()
-
+    for epoch in range(EPOCH):
         pred_boxes, target_boxes = get_bboxes(
             train_loader, model, iou_threshold=0.5, threshold=0.4
         )
+        # see additional youtube video
 
-        mean_avg_prec = mean_average_precision(
+        mean_average_prec = mean_average_precision(
             pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
         )
-        print(f"Train mAP: {mean_avg_prec}")
 
-        #if mean_avg_prec > 0.9:
-        #    checkpoint = {
-        #        "state_dict": model.state_dict(),
-        #        "optimizer": optimizer.state_dict(),
-        #    }
-        #    save_checkpoint(checkpoint, filename=LOAD_MODEL_FILE)
-        #    import time
-        #    time.sleep(10)
-
-        train_fn(train_loader, model, optimizer, loss_fn)
+        print(f"Train mAP: {mean_average_prec}")
 
 
-if __name__ == "__main__":
+        train__fn(train_loader, model, optimizer, loss_fn)
+
+if __name__== "__main__":
     main()
